176c1c94b0f24d6d899026001a7b7832c847750b
HOPE <habibashokr49@gmail.com>
2023-11-05 17:26:46 +0200

    edite

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:moviesapp/firebase_options.dart';
 import 'package:moviesapp/layout/home_layout.dart';
 import 'package:moviesapp/screens/category_movies.dart';
-import 'package:moviesapp/screens/splash_screen.dart';
 import 'package:moviesapp/shared/styles/themes.dart';
 
 void main() async {
@@ -25,11 +24,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'MoviesApp',
       debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreen.routeName,
+      initialRoute: HomeLayout.routeName,
       routes: {
         HomeLayout.routeName: (context) => HomeLayout(),
         CategoryMoviesScreen.routeName: (context) => CategoryMoviesScreen(),
-        SplashScreen.routeName: (context) => const SplashScreen(),
       },
       theme: MyThemeData.lightTheme,
     );
